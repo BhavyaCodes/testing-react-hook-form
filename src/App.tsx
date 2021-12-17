4d80bb1c36aa0acbb5465095b5920a8301db1f54
BhavyaCodes <xbhavyatomar@gmail.com>
2021-12-17 10:55:54 +0530

    basic tutorial complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+// import { TextField } from "@mui/material";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface IFormInputs {
+  name: string;
+  password: string;
+  age: number;
+}
 
 function App() {
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<IFormInputs>({
+    defaultValues: {
+      name: "hello",
+      password: "",
+    },
+  });
+
+  // console.log(watch());
+  console.log("errors", errors);
+
+  const onSubmit: SubmitHandler<IFormInputs> = ({ name, password, age }) => {
+    // alert("submit");
+    console.log("name", name);
+    console.log("password", password);
+    console.log("age", age);
+    // console.log(typeof age);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="App" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <input
+          {...register("name", { minLength: 3, required: "this is required" })}
+          type="text"
+        />
+        {errors.name && <p>{errors.name.message}</p>}
+      </div>
+      <div>
+        <input
+          {...register("password", { required: "this is required" })}
+          type="password"
+        />
+      </div>
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <div>
+        <input
+          {...register("age", {
+            valueAsNumber: true,
+            max: { value: 100, message: `age can't be more than 100` },
+          })}
+        />
+      </div>
+      {errors.age && <p>{errors.age.message}</p>}
+
+      {/* <TextField
+        inputRef={register}
+        name="name"
+        error
+        id="standard-error-helper-text"
+        label="name"
+        defaultValue="Hello World"
+        helperText="Incorrect entry."
+        variant="standard"
+      /> */}
+      {/* <TextField
+        inputRef={register}
+        name="password"
+        error
+        type="password"
+        id="pwd"
+        label="password"
+        defaultValue="Hello World"
+        helperText="Incorrect entry."
+        variant="standard"
+      /> */}
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
